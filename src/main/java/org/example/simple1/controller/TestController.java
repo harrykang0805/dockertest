66671354b987eadbbb2b6aef02b4e9a006a83456
harry <hw0187@naver.com>
2024-07-19 09:33:31 +0900

    올라리요!

--- src/main/java/org/example/simple1/controller/TestController.java
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
     @GetMapping("/")
     public MemberDto test() {
         MemberDto memberDto = new MemberDto();
-        memberDto.setName("갓성진 킹수정 홍신 갓지");
+        memberDto.setName("갓성진 킹수정 홍신 갓지 짱!");
         memberDto.setAge(1234);
 
         memberRepositoty.save(new Member("member1"));
